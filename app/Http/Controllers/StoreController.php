9745bc6db87654f80d5a65109272516d59431ad6
mantraljay <jayocumen@gmail.com>
2020-02-04 09:48:10 +0800

    Fixed the cross origin CORS

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -30,8 +30,8 @@ class StoreController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'user_id' => 'required',
+      $this->validate($request, [
+        'user_id' => 'required',
             'name' => 'required',
             'address' => 'required',
             'phone' => 'required',
@@ -66,15 +66,12 @@ class StoreController extends Controller
      */
     public function update(Request $request, Store $store)
     {
-        if ($request->user()->id !== $store->user_id && $request->user()->category !== 1) {
-
-            return response()->json(['error' => 'You can only edit your own Store.'], 403);
-    
-          }
-    
-          $store->update($request->only(['name','address', 'phone']));
-    
-          return new StoreResource($store);    }
+        // if ($request->user()->id !== $store->user_id && $request->user()->category !== 1) {
+        //   return response()->json(['error' => 'You can only edit your own Store.'], 403);
+        // }
+        $store->update($request->only(['name','address', 'phone']));
+        return new StoreResource($store);    
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -84,10 +81,10 @@ class StoreController extends Controller
      */
     public function destroy(Request $request,Store $store)
     {
-      if($request->user()->category !== 1){
-        return response()->json(['error' => 'Please contact admin.'], 403);
-      }
-        $store ->delete();
-        return response()->json(null,204);
+      // if($request->user()->category !== 1){
+      //   return response()->json(['error' => 'Please contact admin.'], 403);
+      // }
+      $store ->delete();
+      return response()->json(null,204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
 Route::get('logout', 'AuthController@logout');
 Route::get('user', 'AuthController@getAuthUser');
-
 Route::apiResource('stores', 'StoreController');
